5df21e14412390ed4aac653f094829e572b93389
nataliepham6720 <112508461+nataliepham6720@users.noreply.github.com>
2022-10-13 18:13:14 -0400

    fixing format 5

--- script.py
+++ script.py
@@ -55,23 +55,23 @@ def main():
     else:
         df = pd.DataFrame(columns=['Start Date', 'End Date', 'Forecast'])
     temp = pd.DataFrame({'Start Date': period['startTime'],
-                            'End Date': period['endTime'],
-                            'Forecast': period['detailedForecast']})
+                        'End Date': period['endTime'],
+                        'Forecast': period['detailedForecast']})
     df = pd.concat([df, temp])
     df = df.drop_duplicates()
     df.to_pickle(file)
     # sort repositories
     file = open("README.md", "w")
     file.write('![Status](https://github.com/nataliepham6720/' +
-                'python-get-forecast/actions/workflows/build.yml/'+
+                'python-get-forecast/actions/workflows/build.yml/' +
                 'badge.svg)\n')
     file.write('![Status](https://github.com/nataliepham6720/' +
-                'python-get-forecast/actions/workflows/pretty.yml/' +
-                'badge.svg)\n')
+            'python-get-forecast/actions/workflows/pretty.yml/' +
+            'badge.svg)\n')
     file.write('# Pittsburgh Nightly Forecast\n\n')
     file.write(df.to_markdown(tablefmt = 'github'))
-    file.write('\n\n---\nCopyright © 2022 Pittsburgh Supercomputing Center.'+
-                'All Rights Reserved.')
+    file.write('\n\n---\nCopyright © 2022 Pittsburgh Supercomputing Center.' +
+            'All Rights Reserved.')
     file.close()
 
 
